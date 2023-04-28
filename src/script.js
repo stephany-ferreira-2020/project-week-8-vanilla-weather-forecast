3c310bec7ecc83bd000401b95115e6cb3d155736
Stephany Ferreira <stephany.ferreira@africainternetgroup.local>
2023-04-28 17:33:33 +0100

    added forecast data using API

--- src/script.js
+++ src/script.js
@@ -25,28 +25,50 @@ function formatDate(timestamp) {
 
 // Forecast:
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000)
+  let day = date.getDay()
+  let days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+
+  return days[day]
+}
+
 function displayForecast(response) {
+  let forecast = response.data.daily
+
   console.log(response.data.daily)
+
   let forecastElement = document.querySelector('#forecast')
 
   let forecastHTML = `<div class="row">`
-  let days = ['Thu', 'Fri', 'Sat', 'Sun']
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `<div class="col-2">
-              <div class="weather-forecast-date">${day}</div>
+
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `<div class="col-2">
+              <div class="weather-forecast-date">${formatDay(
+                forecastDay.time
+              )}</div>
+             
               <img
                 class="weather-forecast-icon"
-                src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/broken-clouds-day.png"
+                src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${
+                  forecastDay.condition.icon
+                }.png"
                 alt=""
                 width="70"
               />
               <div class="weather-forecast-temperatures">
-                <span class="weather-forecast-max">18º</span>
-                <span class="weather-forecast-min">12º</span>
+                <span class="weather-forecast-max">${Math.round(
+                  forecastDay.temperature.maximum
+                )}º</span>
+                <span class="weather-forecast-min">${Math.round(
+                  forecastDay.temperature.minimum
+                )}º</span>
               </div>
             </div>`
+    }
   })
 
   forecastHTML = forecastHTML + `</div>`
